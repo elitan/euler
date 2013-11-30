74a3ce94d19391b96088262d511474ade7846d7b
elitasson <johan@eliasson.me>
2013-11-30 22:14:07 +0100

    fixing functions

--- functions.py
+++ functions.py
@@ -1,11 +1,31 @@
+import math as m
+
 def primeFac(n):
+	i = 2
 	arr = []
 
 	while i < m.sqrt(n) + 1:
 		while n % i == 0:
 			arr.append(i)
 			n = n / i
-		 
+	     
 		i += 1
 
+	arr.append(int(n))
+
 	return arr
+
+
+def devisors(n):
+	dev = 0
+	for i in range(2, int(m.ceil(n**0.5))):
+		if n % i == 0:
+
+			dev += 1
+
+	#*2 + 1 and n
+	dev = dev * 2 + 2
+
+	#if perfect square
+		#dev += 1
+	return dev
