7ebaebf31e5e4ede189c6ce9830682f993972c08
elitasson <johan@eliasson.me>
2013-12-03 12:27:06 +0100

    done, fixed functions

--- functions.py
+++ functions.py
@@ -15,6 +15,20 @@ def primeFac(n):
 
 	return arr
 
+def isPrime(n):
+	if n == 2 or n == 3: return True
+	if n < 2 or n%2 == 0: return False
+	if n < 9: return True
+	if n%3 == 0: return False
+	r = int(n**0.5)
+	f = 5
+	while f <= r:
+		#print(f)
+		if n%f == 0: return False
+		if n%(f+2) == 0: return False
+		f +=6
+	return True    
+
 
 def devisorsOld(n):
 	dev = 0
