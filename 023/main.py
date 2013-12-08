9116a3338e5432fa735139d3781404645b90d9ce
Johan Eliasson <johan@eliasson.me>
2013-12-08 19:08:27 +0100

    not started

--- 023/main.py
+++ 023/main.py
@@ -9,4 +9,3 @@ As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest numb
 
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 """
-
