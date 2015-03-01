14d69600dbc9fc0515e20311476d973dd16a6529
elitasson <johan@eliasson.me>
2015-03-01 15:22:32 +0100

    done

---
+++ 073/main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+
+If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+It can be seen that there are 3 fractions between 1/3 and 1/2.
+
+How many fractions lie between 1/3 and 1/2 in the sorted set of reduced proper fractions for d ≤ 12,000?
+"""
+def farey( n, asc=True ):
+    """Python function to print the nth Farey sequence, either ascending or descending."""
+    if asc: 
+        a, b, c, d = 0, 1,  1  , n     # (*)
+    else:
+        a, b, c, d = 1, 1, n-1 , n     # (*)
+    #print "%d/%d" % (a,b)
+    ss = False
+    s = 0
+    while (asc and c <= n) or (not asc and a > 0):
+        k = int((n + b)/d)
+        a, b, c, d = c, d, k*c - a, k*d - b
+
+        if ss and a == 1 and b == 2:
+            return s
+        if ss:
+            s += 1
+        if not ss and a == 1 and b == 3:
+            ss = True
+        #print "%d/%d" % (a,b)
+
+f = farey(12000)
+
+print(f)
