c85ace3197958e75c43d001b1cd10e7a49dd3d41
elitasson <johan@eliasson.me>
2015-03-04 17:33:25 +0100

    optimized isPrime

--- 010/main.py
+++ 010/main.py
@@ -9,18 +9,18 @@ Find the sum of all the primes below two million.
 import math as m
 
 def isPrime(n):
-
-	if n == 2:
-		return True
-
-	if n % 2 == 0:
-		return False
-
-	for i in range(3, int(m.sqrt(n))+1):
-		if n % i == 0:
-			return False
-
-	return True
+	if n == 2 or n == 3: return True
+	if n < 2 or n%2 == 0: return False
+	if n < 9: return True
+	if n%3 == 0: return False
+	r = int(n**0.5)
+	f = 5
+	while f <= r:
+		#print(f)
+		if n%f == 0: return False
+		if n%(f+2) == 0: return False
+		f +=6
+	return True 
 
 primesum = 0
 for i in range(2, 2000000):
