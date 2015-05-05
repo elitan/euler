60fa5b3ed2d69d788cdde15a802e64d18b6dda62
elitasson <johan@eliasson.me>
2015-05-05 23:30:51 +0200

    done

--- 094/main.py
+++ 094/main.py
@@ -9,28 +9,22 @@ We shall define an almost equilateral triangle to be a triangle for which two si
 Find the sum of the perimeters of all almost equilateral triangles with integral side lengths and area and whose perimeters do not exceed one billion (1,000,000,000).
 """
 
-def almostEqualTriArea(x, y):
-	a = int(math.pow(x, 2))
-	b = int(math.pow((y/2),2))
-	ps = a-b
-	if gmpy.is_square(ps):
-		print("Found side:", x)
+def almostEqualTri(x, y):
+	if gmpy.is_square(x**2-(y/2)**2):
+		print("found %d" % x)
 		return x+x+y
 	return 0
 
-def almostEqualTriAreaHandler(x):
-	s = 0
-	s += almostEqualTriArea(x, x+1)
-	s += almostEqualTriArea(x, x-1)
-	return s
+def almostEqualTriHandler(x):
+	return almostEqualTri(x, x+1) + almostEqualTri(x, x-1)
 
 i = 5
 s = 0
 while 3*i+1 < 10**int(sys.argv[1]): 
-	res = almostEqualTriAreaHandler(i)
-	if res > 0:
-		s += res
-		print("current sum:", s)
+	r = almostEqualTriHandler(i)
+	if r > 0:
+		s += r
+		print("current sum: %d" % s)
 		print("")
 	i += 2
-print("Answer:", s)
+print("Answer: %d" % s)
