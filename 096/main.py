84dcdc155e026766dfac2d7361c49a5d18389775
elitasson <johan@eliasson.me>
2015-07-02 23:40:11 +0200

    done

--- 096/main.py
+++ 096/main.py
@@ -6,7 +6,15 @@ A well constructed Su Doku puzzle has a unique solution and can be solved by log
 The 6K text file, sudoku.txt (right click and 'Save Link/Target As...'), contains fifty different Su Doku puzzles ranging in difficulty, but all with unique solutions (the first puzzle in the file is the example above).
 
 By solving all fifty puzzles find the sum of the 3-digit numbers found in the top left corner of each solution grid; for example, 483 is the 3-digit number found in the top left corner of the solution grid above.
+
+(added extra bottom row)
 """
+
+"""
+Solved using backtracking method. Find all possibilitys and start testing lowest set. if wrong path, backtrack.
+"""
+
+
 import sys
 import time
 import copy
@@ -100,6 +108,85 @@ def printSudo(sudo):
 			sys.stdout.write("%s \t" % sudo[row][col].getPossible())
 		sys.stdout.write("\n")
 
+def checkSudo(sudo):
+
+	#print("#########")
+	#print("Checking")
+	#sudo[0][0] = Node(3,0,0)
+	#printSudo(sudo)
+	#print("#########")
+	# check simle len
+	for row in range(0, 9):
+		for col in range(0, 9):
+			if sudo[row][col].len() != 1:
+				print(sudo[row][col].len())
+				print("F 1")
+				return False
+
+	# check row
+	for row in range(0, 9):
+		s = set()
+		for col in range(0, 9):
+			if sudo[row][col].number() in s:
+				print(row, col)
+				print("F 2")
+				return False
+			s.add(sudo[row][col].number())
+
+	#check col
+	for col in range(0, 9):
+		s = set()
+		for row in range(0, 9):
+			if sudo[row][col].number() in s:
+				print("F 2")
+				return False
+			s.add(sudo[row][col].number())
+
+	for row in range(0, 9, 3):
+		for col in range(0, 9, 3):
+			s = set()
+			for rowIn in range(0, 3):
+				for colIn in range(0, 3):
+					if sudo[row+rowIn][col+colIn].number() in s:
+						print("F 2")
+						return False
+					s.add(sudo[row][col].number())			
+
+	return True
+	#check squares
+
+def possibleNumbers(sudo, row, col):
+	global fullNrSet
+
+	self = sudo[row][col]
+	if sudo[row][col].done():
+		return 1
+
+	foundNr = set()
+	# check row
+	for i in range(0,9):
+		n = sudo[row][i]
+		if n != self and n.done():
+			foundNr.add(n.number())
+	# check col
+	for i in range(0,9):
+		n = sudo[i][col]
+		if n != self and n.done():
+			foundNr.add(n.number())
+	# check inside square
+	rowStart = row - (row % 3)
+	colStart = col - (col % 3)
+	for r in range(rowStart, rowStart+3):
+		for c in range(colStart, colStart+3):
+			n = sudo[r][c]
+			if n != self and n.done():
+				foundNr.add(n.number())
+	if self.possible != (fullNrSet - foundNr) and len(fullNrSet - foundNr) != 0:
+		self.newSet(fullNrSet - foundNr)
+		return 2 # new poss
+	elif len(fullNrSet - foundNr) == 0:
+		return 3 # wrong, backtrack
+	return 1 #no changes where made
 
 def solveSimple(sudo):
 	reRun = True
@@ -108,57 +195,46 @@ def solveSimple(sudo):
 		for row,r in enumerate(sudo):
 			for col,node in enumerate(r):
 				#print(node)
-				r = node.possibleNumbers(sudo) 
-				if not r:
-					#printSudo(sudo)
-					#sys.exit();
-					return False
-					#print("do again!")
-					reRun = True
+				r = possibleNumbers(sudo, row, col) 
+				if r == 3:
+					return False # backtrack
+				if r != 1:
+					reRun = True # something changed, lets make sure we do possibleNumbers again
+	return True # no solution found, try next set
 
-				#print(node)
-				#print("")
-				#time.sleep(0.4)
-				#sys.exit()
-	print(r)
-	#print("We need to backtrack this sudoku!");
-	return True # no solution found?
+def solveSudoko(sudo, depth = 0):
+	r = solveSimple(sudo)
+	if not r:
+		return r, None
 
-def solveSudoko(sudo, rowStart = 0, colStart = 0):
-	for row in range(rowStart, 9):
-		for col in range(colStart, 9):
-			print("######")
-			print(row, col)
-			print("######")
-			r = solveSimple(sudo)
-			current_node = sudo[row][col]
-			print(row, col, r)
+	# find the node with smallest set
+	rowLow, colLow, low = 0, 0, 11
+	for row in range(0, 9):
+		for col in range(0, 9):
+			n_len = sudo[row][col].len()
+			if n_len > 1 and n_len < low:
+				rowLow = row
+				colLow = col
+				low = n_len
 
-			if current_node.len() == 1:
-				continue
+	# if just one item in each node
+	if low == 11:
+		if checkSudo(sudo):
+			return True, sudo # win
+		else:
+			return False, None
 
-			if not r:
-				printSudo(sudo)
-				print(current_node)
-				return False
+	row = rowLow
+	col = colLow
+	for i, c in enumerate(sudo[row][col].possible):
+		sudoTmp = copy.deepcopy(sudo)
+		sudoTmp[row][col] = Node(c, row, col)
+		r = solveSudoko(sudoTmp, depth+1)
+		if r[0]:
+			return True, r[1]
 
-			#time.sleep(.5)
-
-				
-			for c in current_node.getPossible():
-				print("Currently on")
-				print(row, col, c, current_node.getPossible())
-				sudo[row][col] = Node(c, row, col)
-				r = solveSudoko(copy.deepcopy(sudo))
-				print("r was " , r)
-				if not r:
-					print("false")
-					printSudo(sudo)
-					#sys.exit()					
-					print("continue 2")
-					continue
-	printSudo(sudo)
-
+	return False, None
+		
 
 # parse text
 c = 0
@@ -181,7 +257,15 @@ for l in open("p096_sudoku.txt", "r"):
 sudoArr.pop(0)
 
 # solve soduko
-for sudo in sudoArr:
-	solveSudoko(sudo)
-	print("done??")
-	sys.exit()
+c = 0
+for i, sudo in enumerate(sudoArr):
+	r, sudoTmp = solveSudoko(sudo)
+	print("%d solved" % (i+1))
+	nr = int("%d%d%d" % (sudoTmp[0][0].number(), sudoTmp[0][1].number(), sudoTmp[0][2].number()))
+	print(nr)
+	
+	c += int("%d%d%d" % (sudoTmp[0][0].number(), sudoTmp[0][1].number(), sudoTmp[0][2].number()))
+		
+	#raw_input("next")
+	#sys.exit()
+print(c)
