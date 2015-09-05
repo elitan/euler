d26ec0b8f8d6892f53ed5164edf990249b07a269
elitasson <johan@eliasson.me>
2015-09-05 22:53:42 +0200

    dynamic programming

--- 076/main.py
+++ 076/main.py
@@ -12,37 +12,12 @@ How many different ways can one hundred be written as a sum of at least two posi
 """
 
 import sys
-sys.path.append("../")
-import functions as f
-import itertools
-import copy
 
-# global
-count = 0
+target = 100
+available_value = list(xrange(target))[1:]
+ways = [1] + [0]*target
 
-def minimize(nl, m=0):
-	global count
-
-	nnl = copy.deepcopy(nl)
-	if len(nnl) < 2:
-		return False
-
-	while len(nnl) >= m+2:
-		nnl[m] += nnl[m+1]
-		del nnl[m+1]
-		
-		if m == 0 or nnl[m] <= nnl[m-1]:
-			print(nnl)
-			count += 1
-			minimize(nnl, m+1)
-
-n = int(sys.argv[1])
-nl = list()
-
-# prepare list
-for i in range(0, n):
-	nl.append(1)
-
-minimize(nl)
-
-print(count)
+for value in available_value:
+	for i in range(value, target+1):
+		ways[i] += ways[i-value]
+print(ways[target])
